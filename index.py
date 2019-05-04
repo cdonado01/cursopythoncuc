c12a3df3b9953b7275ffc411ed0f4148940d3f71
Android Sala 15 <android@cuc.edu.co>
2019-05-04 10:43:28 -0500

    funciones y vectores

--- index.py
+++ index.py
@@ -130,16 +130,95 @@ print("Es mayor de edad") if (edad >= 18) else print("Es menor de edad")
 # Si de linea sin sino
 if (edad >= 18): print("Es mayor de edad")
 
+# Arrays
 
+v = ["Hola", "Mundo", 4, 3.4, True, ["Otro", "Array"]]
+v = v[0:3] # Toma el vector desde la posición cero a la posición 3
+print(v[0])
+v2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+v3 = range(1,11) # Lo mismo que el anterior, no toma el ultimo valor
+v4 = range(11)
 
+# Recorrer vector
+for x in v3:
+    print(x)
+    
+# Eliminar elemento
+v5 = ["Hola", "Carlos", 1]
 
+v5.remove("Hola") # Elimina lo que diga hola
+v5.pop() # Elimina la ultima pocición
+v5.pop(1) # Elimina la posicion x, en este caso 1
+v5.clear() # Elimina todo el vector
 
+# Agregar Elementos
+v5.append(5) # Agrega 5 en la ultima posición
+v5.insert(2, "Donado") # Agrega donado en la posición 2
 
+# Conoce la posición de un elemento
+v5.index("Carlos") # Retorna la posisción de Carlos en el vector
 
+print(len(v5)) # Retirna el numero de elementos del vector
 
+v5.count("Carlos") # Cuenta el numero de registro que digan Carlos en el vector
 
+# Ordenar un vector
+a = [5, 2, 7, 0]
+a.sort()
 
+# Recorrer Saltando
+for x in a[0:4:2]: # Recorre desde cero hasta cuatro saltando de dos en dos
+    print(x)
+    
+for x in a[0::2]: # Recorre desde cero hasta el final saltando de dos en dos
+    print(x)
 
+res = 2 in a
+
+a[-1] # Trae el ultimo elemento
+a[-2] # Trae el penultimo elemento
+    
+# Matriz
+m = [[2, 4], [4, 2]]
+
+# Ejercicio sumatoria
+
+print("Sumatoria de: ")
+val = int(input())
+
+vsum = range(val + 1)
+suma = 0
+for x in vsum:
+    suma = suma + x
+    
+print(suma)
+
+# Otra forma de sumatoria
+print("la sumatoria es ", sum(range(val+1)))
+
+# While - Mientras que
+
+i = 1
+while i<5:
+    print(i)
+    i = i + 1
+
+# Procedimientos
+def hola_mundo():
+    print("Hola mundo")
+
+# Funcionaes
+def elevar_cuadrado(numero):
+    return numero ** 2
+
+def elevar_exponente(numero, exponente=2): # Aqui defino parametros por defecto
+    return numero ** exponente
+
+# Invocar funciones y metodos
+hola_mundo()
+elevar_cuadrado(5)
+elevar_exponente(5)
+elevar_exponente(5, 3)
 
 
 
